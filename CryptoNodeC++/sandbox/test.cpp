b8825d6bbaa6a14208de0025455d153bbe610eef
Bartosz Nowak <81524999+Okm165@users.noreply.github.com>
2021-08-05 20:30:35 +0200

    Add files via upload

--- CryptoNodeC++/sandbox/test.cpp
+++ CryptoNodeC++/sandbox/test.cpp
@@ -2,12 +2,11 @@
 
 int main()
 {
-    Dict paths = composeJsonDict("../config/paths.json");
-    Index index(paths["index"].c_str());
     AddressDecoder addrdec;
-    std::string hash (HexToStringReversed("000000000000000000129da18d7db6b158d55b23f7386fe8dd6b7cfdb90de71a"));
-    BBlock bblock = readBBlock(&index, &addrdec, paths["blocks"].c_str(), hash);
-    std::cout << bblock.print() << std::endl;
-    RBlock rblock = readRBlock(&index, &addrdec, paths["blocks"].c_str(), hash);
-    std::cout << rblock.print() << std::endl;
+    std::string pkscript(HexToString("232102ad5d7d59d2072bda607bd06e61f4e9b37b07304785bb44352a495b617166f9f3ac"));
+    std::cout << pkscript.size() << std::endl;
+    Address addr_buf;
+    BStream stream(&pkscript);
+    addrdec.addressDecode(&addr_buf, &stream, SCRIPT);
+    std::cout << addr_buf.print() << std::endl;
 }
